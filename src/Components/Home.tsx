4dc2ea9ef26471c256c1ebfab03b2944715cbcdc
Lucas Santos <lucas.silva1203@outlook.com>
2023-01-18 10:11:16 -0300

    Modify webpage design

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,11 +1,20 @@
-import styles from "./Home.module.css"
+import { ArrowRight, FileArrowDown } from "phosphor-react";
+import styles from "./Home.module.css";
 
 export const Home = () => {
     return (
-        <div id="home" className={styles["portfolio-home"]}>
+        <section id="home" className={styles["portfolio-home"]}>
             <div className={styles["home-greetings"]}>
                 Ola, me chamo lucas!
+                <div className={styles["greetings-buttons"]}>
+                    <button className={styles["project-button"]}>
+                        See my projects<ArrowRight size={18} />
+                    </button>
+                    <button className={styles["curriculum-button"]}>
+                        Download CV<FileArrowDown size={18} />
+                    </button>
+                </div>
             </div>
-        </div>
+        </section>
     );
 };
