73b915160cf4e2ffdb2dd41098a9bbf6ebc8e382
Lucas Santos <lucas.silva1203@outlook.com>
2023-01-24 21:15:48 -0300

    Modify gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import "./global.css";
 
 function App() {
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
