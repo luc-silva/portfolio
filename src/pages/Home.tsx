3430608e30732203e92270c9699c92d216ef17bd
Lucas Santos <lucas.silva1203@outlook.com>
2023-02-27 17:48:23 -0300

    Remove footer, old images and fix css variable

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,17 +38,17 @@ export const Home = () => {
                 <div className={styles["social-accounts"]}>
                     <ul>
                         <li>
-                            <a href="" target={"_blank"}>
+                            <a href="https://www.linkedin.com/in/silva-luc/" target={"_blank"}>
                                 <LinkedinLogo size={30} color="var(--main-color)" />
                             </a>
                         </li>
                         <li>
-                            <a href="" target={"_blank"}>
+                            <a href="https://github.com/luc-silva" target={"_blank"}>
                                 <GithubLogo size={30} color="var(--main-color)" />
                             </a>
                         </li>
                         <li>
-                            <a href="" target={"_blank"}>
+                            <a href="mailto:lucas.silva1203@outlook.com">
                                 <Envelope size={30} color="var(--main-color)" />
                             </a>
                         </li>
