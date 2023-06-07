124cd414442111a075295d1eee420e8bdf3e04f0
Lucas Santos <lucas.silva1203@outlook.com>
2023-06-07 14:53:13 -0300

    Update about me text

--- src/components/Texts/AboutMeText.tsx
+++ src/components/Texts/AboutMeText.tsx
@@ -3,17 +3,21 @@ export const AboutMeText = () => {
         <>
             <p>
                 Atualmente cursando o terceiro semestre, busco por uma
-                oportunidade de estágio na área de desenvolvimento. Utilizo
-                Javascript como minha principal linguagem de programação,
-                estando atualmente aprendendo React em conjunto com Typescript e
-                ainda pretendo aprender Java backend futuramente.
+                oportunidade de estágio ou posição como júnior na área de
+                desenvolvimento. Utilizo Typescript, React e MongoDB como minhas
+                principais ferramentas, mas também tenho alguns projetos em Java
+                + MySQL e estudo springboot, como o objetivo de aprofundar os
+                estudos em backend.
             </p>
             <p>
                 Desde pequeno era fã do mundo de desenvolvimento de software, em
                 especial a área de jogos. Em 2016 cheguei a cursar um curso de
-                desenvolvimento de games, tendo o concluido em 2018. Não possuo
-                experiência de trabalho e nem como freelancer, mas sempre tento
-                me desafiar ao aplicar novas implementações em meus projetos.
+                desenvolvimento de games, tendo o concluido em 2018.
+            </p>
+            <p>
+                Não possuo experiência de trabalho e nem como freelancer, mas
+                sempre tento me desafiar ao aplicar novas implementações em meus
+                projetos.
             </p>
         </>
     );
